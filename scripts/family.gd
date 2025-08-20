1f1568e2565ea2de5a7fb5c85cd80c3bfdecc867
JanosSandor2002 <sandorj2002@gmail.com>
2025-08-20 14:53:00 +0200

    javítva

--- scripts/family.gd
+++ scripts/family.gd
@@ -1,44 +1,28 @@
 extends Node
 
 # Family datas
-var mother_hunger: int
-var child1_hunger: int
-var child2_hunger: int
-var child3_hunger: int
+var family_member_name: String
+var hunger: int
 
 # Constructor
-func _init(m_hunger: int, c1_hunger: int, c2_hunger: int, c3_hunger: int):
-	mother_hunger = m_hunger
-	child1_hunger = c1_hunger
-	child2_hunger = c2_hunger
-	child3_hunger = c3_hunger
+func _init(name: String, h: int):
+	family_member_name = name
+	hunger = h
 
 # Getters
-func get_mother_hunger() -> int:
-	return mother_hunger
+func get_family_member_name() -> String:
+	return family_member_name
 
-func get_child1_hunger() -> int:
-	return child1_hunger
-
-func get_child2_hunger() -> int:
-	return child2_hunger
-
-func get_child3_hunger() -> int:
-	return child3_hunger
+func get_hunger() -> int:
+	return hunger
 
 # Setters
-func set_mother_hunger(val: int) -> void:
-	mother_hunger = val
+func set_family_member_name(name: String) -> void:
+	family_member_name = name
 
-func set_child1_hunger(val: int) -> void:
-	child1_hunger = val
-
-func set_child2_hunger(val: int) -> void:
-	child2_hunger = val
-
-func set_child3_hunger(val: int) -> void:
-	child3_hunger = val
+func set_hunger(val: int) -> void:
+	hunger = val
 
 # toString / description
 func get_description() -> String:
-	return "Mother Hunger: %d | Child1 Hunger: %d | Child2 Hunger: %d | Child3 Hunger: %d" % [mother_hunger, child1_hunger, child2_hunger, child3_hunger]
+	return "Family Member: %s | Hunger: %d" % [family_member_name, hunger]
--- tests/test_family.gd
+++ tests/test_family.gd
@@ -3,31 +3,25 @@ extends GutTest
 const Family = preload("res://scripts/family.gd")
 
 func test_description():
-	var fam = Family.new(50, 20, 30, 40)
+	var Test1 = Family.new("Smith", 50)
 	
 	# get_description test
-	var desc = fam.get_description()
-	assert_eq(desc, "Mother Hunger: 50 | Child1 Hunger: 20 | Child2 Hunger: 30 | Child3 Hunger: 40")
+	var desc = Test1.get_description()
+	assert_eq(desc, "Family Member: Smith | Hunger: 50")
 
 func test_getters():
-	var fam = Family.new(50, 20, 30, 40)
+	var fam = Family.new("Smith", 50)
 
 	# Getter tests
-	assert_eq(fam.get_mother_hunger(), 50)
-	assert_eq(fam.get_child1_hunger(), 20)
-	assert_eq(fam.get_child2_hunger(), 30)
-	assert_eq(fam.get_child3_hunger(), 40)
+	assert_eq(fam.get_family_member_name(), "Smith")
+	assert_eq(fam.get_hunger(), 50)
 
 func test_setters():
-	var fam = Family.new(50, 20, 30, 40)
+	var fam = Family.new("Smith", 50)
 
 	# Setter tests
-	fam.set_mother_hunger(60)
-	fam.set_child1_hunger(25)
-	fam.set_child2_hunger(35)
-	fam.set_child3_hunger(45)
+	fam.set_family_member_name("Johnson")
+	fam.set_hunger(75)
 
-	assert_eq(fam.get_mother_hunger(), 60)
-	assert_eq(fam.get_child1_hunger(), 25)
-	assert_eq(fam.get_child2_hunger(), 35)
-	assert_eq(fam.get_child3_hunger(), 45)
+	assert_eq(fam.get_family_member_name(), "Johnson")
+	assert_eq(fam.get_hunger(), 75)
