59eecf4469c4909c09cea260e4ecec43c69ef693
Sebe Zsolt <sebezsolt2@gmail.com>
2025-08-22 14:54:54 +0200

    work on menu system

--- scenes/menu/main_menu.gd
+++ scenes/menu/main_menu.gd
@@ -14,12 +14,12 @@ func _ready() -> void:
 	_load_translations()
 
 func _on_play_pressed() -> void:
-	# get_tree().change_scene_to_file("...")
-	print("start game")
+	get_tree().change_scene_to_file("res://scenes/menu/save_select_menu.tscn")
 
 
 func _on_settings_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/menu/settings_menu.tscn")
+	var settings := preload("res://scenes/menu/settings_menu.tscn").instantiate()
+	add_child(settings)
 
 
 func _on_quit_pressed() -> void:
