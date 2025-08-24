001803a28520e43927fbb2474ba44e03c985f421
Sebe Zsolt <sebezsolt2@gmail.com>
2025-08-24 16:52:23 +0200

    add save select menu

--- scenes/menu/save_select_menu.gd
+++ scenes/menu/save_select_menu.gd
@@ -8,8 +8,6 @@ extends Control
 
 @onready var SAVE_LIST := $MarginContainer/VBoxContainer/ScrollContainer/SaveList
 
-
-
 func _load_translations() -> void:
 	TITLE.text = tr(&"LVL_SELECT_MENU_TITLE")
 	NEW_SAVE_BTN.text = tr(&"LVL_SELECT_MENU_NEW_SAVE")
@@ -17,7 +15,24 @@ func _load_translations() -> void:
 
 func _ready() -> void:
 	_load_translations()
+	
+	for save in SaveManager.get_list_of_save_files():
+		print(save.name)
+		var entry := SaveEntry.create(save)
+		
+		entry.select_requested.connect(_on_save_select_requested)
+		entry.connect(&"delete_requested", _on_save_delete_requested)
+		
+		SAVE_LIST.add_child(entry)
 
+func _on_save_select_requested(info: SaveManager.SaveInfo) -> void:
+	print("selected save: ", info.name)
+	SaveManager.load_save_file(info.file) # TODO: handle errors
+	# this is where the game.gd supposed to take control
+	#get_tree().change_scene_to_file("...")
+
+func _on_save_delete_requested(info: SaveManager.SaveInfo) -> void:
+	print("delete save: ", info.name)
 
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menu/main_menu.tscn")
